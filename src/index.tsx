ccc96016174b959d875b60cc1dd1911c48702f8c
wSedlacek <wsedlacekc@gmail.com>
2021-02-04 23:24:18 -0700

    refactor: add props
    Signed-off-by: wSedlacek <wsedlacekc@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React, { Component, FC, ReactElement } from 'react';
 
-export const ExampleA: FC = () => {
+export interface ExampleAProps {}
+
+export const ExampleA: FC<ExampleAProps> = () => {
   return <h1>Hello World</h1>;
 };
 
-export class ExampleB extends Component {
+export interface ExampleBProps {}
+
+export class ExampleB extends Component<ExampleBProps> {
   public render(): ReactElement {
     return <span>Another Example</span>;
   }
